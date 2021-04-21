d47fcc5a0a1a44a2c67be7caacc638101823059c
Hyeju Kil <mmow110@naver.com>
2021-04-21 18:09:01 +0900

    1966 PrinterQueue

--- Main.java
+++ Main.java
@@ -2,58 +2,76 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n;
-	static long m;
-	static long inputs[];
    public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
      
-      String imsis[] = br.readLine().split(" "); //첫줄 입력
-      n = Integer.parseInt(imsis[0]);
-      m = Long.parseLong(imsis[1]);
-      
-      imsis = br.readLine().split(" "); //둘째줄 입력
-      inputs = new long[n];
+      //입력
+      int n= Integer.parseInt(br.readLine());
+      int inputNum = 0; //입력받는 문서 수
+      int answer = 0; //몇번째로 출력되는지 알아야하는 문서 번호
+      int count=0; //출력되는 순서
+      String imsis[];
+      Queue<Node> q;
       for(int i=0;i<n;i++)
       {
-    	  inputs[i] = Long.parseLong(imsis[i]);
+    	  count=0;
+    	  imsis = br.readLine().split(" "); //문서 정보 입력
+    	  inputNum = Integer.parseInt(imsis[0]); 
+    	  answer = Integer.parseInt(imsis[1]);
+    	  
+    	  q = new LinkedList<Node>();
+    	  Node imsinode;
+    	  imsis = br.readLine().split(" ");
+    	  for(int j=0;j<inputNum;j++) //문서 입력
+    	  {
+    		  imsinode = new Node(j,Integer.parseInt(imsis[j]));
+    		  q.add(imsinode);
+    	  }
+    	  
+    	  //반복으로 언제 출력되는지 찾기
+    	  while(true)
+    	  {
+    		  boolean isbreak = false;
+    		  Iterator<Node> itr = q.iterator();
+    		  while(itr.hasNext())
+    		  {
+    			  if(itr.next().priority > q.peek().priority) //더 큰 우선순위 발견
+    			  {
+    				  Node imsiPoll = q.poll();
+    				  q.add(imsiPoll); //다시 제일 뒤에 삽입
+    				  isbreak = true;
+    				  break;
+    			  }
+    		  }
+    		  //더 큰 우선순위 발견 실패 -> 찾는 위치의 문서일 경우 끝, 아니면 그냥 poll
+    		  if(isbreak == false)
+    		  {
+    			  count++;
+        		  if(q.peek().input == answer)
+        		  {
+        			  bw.write(count+"\n");
+        			  q.clear();
+        			  break;
+        		  }
+        		  else
+        			  q.poll();  
+    		  }
+    	  }
       }
-      
-      Arrays.sort(inputs);
-     
-      bw.write(BinarySearch(0, inputs[inputs.length-1])+"\n");
       bw.flush();
       
    }
-   
-   
-   
-  static long BinarySearch(long start, long end) //인자는 수
-   {
-	   long mid = (start+end)/2;
-	   long choppedLength =0;
-	   
-	   for(int i=0;i<n;i++) //전체 돌면서 나무자르기
-	   {
-		   if(inputs[i]>mid) //만약 나무가 절단기 높이보다 더 크면 절단하고 잘린 길이 추가
-		   {
-			   choppedLength += inputs[i]-mid;
-		   }
-	   }
-	   if(choppedLength == m) //m미터와 딱 맞을 시
-		   return mid;
-	   if(start >=end) //이미 탐색 범위 탈출
-	   {
-		  if(choppedLength >m) //길이는 충분
-			  return start;
-		  else // 탐색 범위 탈출하고, 길이가 부족할 시 한번 더잘라서 탐색 
-			  return BinarySearch(start-1, start-1);
-	   }
-	   else if(choppedLength <m) //더 잘라야 하는 경우
-		   return BinarySearch(start, mid-1);
-	   else //덜 잘라야 하는 경우
-		   return BinarySearch(mid+1, end);
-   }
+}
+
+class Node{
+	int input;
+	int priority;
+	
+	Node(int input, int priority)
+	{
+		this.input = input;
+		this.priority = priority;
+	}
 }
 
