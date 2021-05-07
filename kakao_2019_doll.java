63000250f8e4d43d36b0ef04652c5c281e71845e
Hyeju Kil <mmow110@naver.com>
2021-05-07 19:34:52 +0900

    2019_카카오 개발자 겨울 인턴십_크레인 인형뽑기 게임

---
+++ kakao_2019_doll.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class kakao_2019_doll {
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+	    Stack<Integer> s = new Stack<Integer>();
+        int answer = 0;
+        for(int i=0;i<moves.length;i++)
+	     {
+	    	  int now = moves[i]-1;
+	    	  for(int j=0;j<board[now].length;j++)
+	    	  {
+	    		  if(board[j][now] == 0) 
+	    			  continue;
+	    		  else //인형 있음
+	    		  {
+	    			  if(s.isEmpty() || s.peek() != board[j][now])
+	    			  {
+	    				  s.push(board[j][now]);
+	    			  }
+	    			  else//만약 같은것 이미 있을 경우
+	    			  {
+	    				  answer+=2;
+	    				  s.pop();
+	    			  }
+	    			  board[j][now] =0; //인형 뺐으니 삭제
+	    			  break;
+	    		  }
+	    	  }
+	     }
+        return answer;
+    }
+}
+}
