8dd391aebc811444c414e5fa09d993b39ae2bd37
Hyeju Kil <mmow110@naver.com>
2021-05-13 11:03:37 +0900

    10953_A+B -6

---
+++ B_10953.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B_10953 {
+	public static void main(String[] args) throws IOException {
+
+	     BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	     
+	     //ют╥б
+	     int n =Integer.parseInt(br.readLine());
+	     
+	     for(int i=0;i<n;i++)
+	     {
+		     String inputs[] = br.readLine().split(",");
+		     bw.write(Integer.parseInt(inputs[0])+Integer.parseInt(inputs[1])+"\n");
+	     }
+	     bw.flush();
+	 }
+}
