3b9afb5afa85e1a8a043fbf99466830f621323fa
Hyeju Kil <mmow110@naver.com>
2021-05-17 11:37:39 +0900

    tomato fail

--- Main.java
+++ Main.java
@@ -3,50 +3,139 @@ import java.io.*;
 import java.math.BigInteger;
 
 public class Main {
+	static int Y;
+	static int X;
+	static int tomato[][];
+	static boolean isvisit[][];
+	static int Acount=0;
+	static Queue<Location> q;
+	static int tomatocount;
 	 public static void main(String[] args) throws IOException {
 
 	     BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	     
-	     StringBuilder gibon = new StringBuilder("IOI");
-	     int n =Integer.parseInt(br.readLine()); //n
+	     //익은 토마토면 -> BFS로 돌면서 1로 바꾸기, count++ (익는 순서가 너비순으로 퍼지므로)
+	     //다 끝났을 시 전체 한번 돌면서 안익은거 있나 확인, 있을시 -1, 없으면 count
 	     
-	     for(int i=1;i<n;i++)
+	     //입력
+	     String inputs[] = br.readLine().split(" ");
+	      X = Integer.parseInt(inputs[0]);
+	      Y = Integer.parseInt(inputs[1]);
+	     tomato = new int[Y][X];
+	     isvisit = new boolean[Y][X];
+	     int answer=0;
+	     
+	     Acount =0;
+	     tomatocount =0; //전체 덜익은 토마토 개수
+	     boolean firstall = true;
+	     
+	     String imsi;
+	     q = new LinkedList<Location>(); //BFS이므로 queue
+	     for(int i=0;i<Y;i++)
 	     {
-	    	 gibon.append("OI");
-	     }
-	     
-	     //s의 길이 m
-	     int m =Integer.parseInt(br.readLine()); //m
-	     String s = br.readLine();
-	     int count=0;
-	     
-	     //s 반복문 돌면서 gibon과 같을 시 count++;
-	     
-	     for(int i=0;i<s.length();i++)
-	     {
-	    	 if(s.charAt(i) == 'O') //시작점이 I이므로 고려하지 않아도 될듯
-	    		 continue;
-	    	 else //I로 시작
-	    	 {
-	    		 if(i+gibon.length() > s.length()) //더이상 없음
-	    			 break;
-	    		 String imsi = s.substring(i, i+gibon.length()); //임시 문자열
-	    		 if(imsi.equals(gibon.toString()))
+	    	 imsi = br.readLine();
+		     StringTokenizer st = new StringTokenizer(imsi);
+		     int j=0; 
+		     while(st.hasMoreTokens())
+		     {
+		    	 tomato[i][j] = Integer.parseInt(st.nextToken());
+	    		 if(tomato[i][j] != 1) //익어있는 상태가 아닐 시
 	    		 {
-	    			 count++; 
+	    			 firstall = false;
+	    			 if(tomato[i][j] == 0)
+	    				 tomatocount++;
+	    				 
 	    		 }
-	    		 
-	    	 }
+	    		 else 
+	    			 q.add(new Location(i,j,0));
+	    		 j++;
+		     }
 	     }
 	     
-	     bw.write(count+"\n");
+	     if(firstall == true) //처음부터 다 익어있는 상태
+	     {
+	    	 bw.write(0+"\n");
+	    	 bw.flush();
+	    	 return;
+	     }
+	     
+	     Location start = q.poll();
+	     BFS(start.y,start.x,start.count);
+	     
+	     
+	     //안익은 토마토 있는지 검사
+	     if(tomatocount != 0)
+	     {
+	    	 bw.write(-1+"\n");
+	     }
+	     else
+	    	 bw.write(Acount+"\n"); //익는데 걸리는 일수
 	     bw.flush();
 	 }
 	 
-	 //알게된 점 : String과 StringBuilder를 equals 연산할 시 toString으로 바꿔줘야 인식함
-	 //시간초과
-	 //수정 -> kmp 알고리즘 사용하기
+	 static void BFS(int y, int x, int count)
+	 {
+		 if(isvisit[y][x] == true || tomato[y][x] == -1) //방문한 노드이거나 토마토 없음
+		 {
+			 if(q.isEmpty())
+				 return;
+			 else
+			 {
+				 Location imsi = q.poll();
+				 BFS(imsi.y,imsi.x,imsi.count);
+			 }
+		 }
+		 else 
+		 {
+			 //System.out.println("y : "+y+" x : "+x +" tomatocount : "+tomatocount);
+			 if(tomato[y][x] == 0)
+				 tomatocount--;
+			 isvisit[y][x] = true;
+			 tomato[y][x] = 1; //익음
+			 if(y>0 && tomato[y-1][x] == 0) //위가 있고 토마토 있을 경우
+			 {
+				 q.add(new Location(y-1,x,count+1));
+			 }
+			 if(y<Y-1 && tomato[y+1][x] == 0) //아래가 있을 경우
+			 {
+				 q.add(new Location(y+1,x,count+1));
+			 }
+			 if(x>0 && tomato[y][x-1] == 0) //왼쪽 있고 토마토 있을 경우
+			 {
+				 q.add(new Location(y,x-1,count+1));
+			 }
+			 if(x<X-1 && tomato[y][x+1] == 0) //오른쪽 있을 경우
+			 {
+				 q.add(new Location(y,x+1,count+1));
+			 }
+		 }
+		 if(Acount < count)
+			 Acount = count;
+		 //다 넣었으면 다음거 
+		if(q.isEmpty())
+		{
+			 return;	
+		}
+		else
+		{
+			Location imsi = q.poll();
+			BFS(imsi.y,imsi.x,imsi.count);
+		}
+	 }
+}
+
+class Location{
+	int y;
+	int x;
+	int count;
+	
+	Location(int y, int x,int count)
+	{
+		this.y = y;
+		this.x = x;
+		this.count = count;
+	}
 }
 
 
