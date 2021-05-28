525f4c7e3b8b940658c2abf8e1c880248ddd2ff7
Hyeju Kil <mmow110@naver.com>
2021-05-28 18:52:06 +0900

    15649_fail

--- Main.java
+++ Main.java
@@ -3,56 +3,53 @@ import java.io.*;
 import java.math.BigInteger;
 
 public class Main {
+	static int n;
+	static int m;
+	static int b[];
+	static boolean isused[];
 	 public static void main(String[] args) throws IOException {
 		 
 	     BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	     
-	     int n = Integer.parseInt(br.readLine());
-	     LinkedList<Character> ans = null;
-	     for(int i=0;i<n;i++)
+	     String inputs[] = br.readLine().split(" ");
+	     n = Integer.parseInt(inputs[0]);
+	     m = Integer.parseInt(inputs[1]);
+	     b = new int[m];
+	     isused = new boolean[n];
+	     
+	     hamsu(0,1);
+	     
+}
+	 static void hamsu(int jarisu, int gap)
+	 {
+	     if(gap>n) //자리수의 값이 n+1 -> 끝일 경우
 	     {
-	    	 String input = br.readLine();
-	    	 char nowin;
-	    	 int now=0;
-	    	 ans = new LinkedList<Character>();
-	    	 for(int j=0;j<input.length();j++)
-	    	 {
-	    		 nowin = input.charAt(j);
-	    		 //System.out.println(nowin+" "+now);
-	    		 if(nowin == '<' && now>0)
-	    		 {
-	    			 now-=1;
-	    		 }
-	    		 else if(nowin == '>' && now<ans.size())
-	    		 {
-	    			 now+=1;
-	    		 }
-	    		 else if(nowin == '-' && !ans.isEmpty())
-	    		 {
-	    				 if(now >= ans.size()) //끝번
-	    				 {
-	    					 ans.removeLast();
-	    					 now-=1;
-	    				 }
-	    				 else
-	    					 ans.remove(now);
-	    		 }
-	    		 else if(nowin != '<' && nowin != '-' && nowin !='>')
-	    		 {
-	    			 ans.add(now, nowin);
-	    			 now+=1;
-	    		 }
-	    	 }
-	    	 
-		     for(int j=0;j<ans.size();j++)
-		    	 bw.write(ans.get(j));
-		     bw.write("\n");
+	    	 //앞자리수 +1로 리턴, isused 갱신
+	    	 if(jarisu == m-1) //끝자리
+	    		 return;
+	    	 isused[b[jarisu-1]] = false; //앞자리수 isued false
+	    	 isused[b[jarisu-1]+1] = true;
+	    	 hamsu(jarisu-1, gap+1);
 	     }
-	     
-	     bw.flush();
-	     
+	     else if(isused[gap] == true) //이미 쓰여지고 있는 경우
+	    	 hamsu(jarisu,gap+1);
+	     else //끝도 아니고, 이미쓰여지고 있지도 않고
+	     {
+	    	 //현재 수 저장, 다음 자리수로 고
+	    	 isused[gap] = true;
+	    	 b[jarisu] = gap;
+	    	 if(jarisu == m-1) //출력
+	    	 {
+	    		 for(int i=0;i<m;i++)
+		    		 System.out.print(b[i]+" ");
+		    	 System.out.println();
+		    	 hamsu(jarisu,gap+1);
+	    	 }
+	    	 hamsu(jarisu+1, 1);
+	     }
 	 }
 }
+
 
 
