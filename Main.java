d767ed5a3e379c5bf8664de35f0d6e20d91b8c69
Hyeju Kil <mmow110@naver.com>
2021-05-31 16:32:52 +0900

    n-queen 미완

--- Main.java
+++ Main.java
@@ -5,70 +5,160 @@ import java.math.BigInteger;
 
 public class Main {
 	static int n;
-	static int m;
-	static int b[];
-	static boolean isvisit[];
+	static boolean chess[][];
+	static boolean sero[];
+	static boolean garo[];
+	static int ans=0;
     static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     
 	 public static void main(String[] args) throws IOException {
 		 
+	     n = Integer.parseInt(br.readLine());
+	    
+	     chess = new boolean[n][n];
+	     sero = new boolean[n];
+	     garo = new boolean[n];
 	     
-	     String inputs[] = br.readLine().split(" ");
-	     n = Integer.parseInt(inputs[0]);
-	     ConcurrentLinkedDeque<Integer> q = new ConcurrentLinkedDeque<Integer>();
-	     m = Integer.parseInt(inputs[1]);
-	     inputs = br.readLine().split(" ");
-	     int ans=0;
-	     int count3=0;
+	     DFS(0,0,0);
 	     
-	     for(int i=1;i<n;i++)
-	    	 q.add(i);
-	     
-	     for(int i=0;i<inputs.length;i++) //뽑아낼 수 갯수만큼
-	     {
-	    	 if(q.isEmpty())
-	    		 break;
-	    	 int su = Integer.parseInt(inputs[i]); //뽑아낼 수
-	    	 int count=0;
-	    	 Iterator<Integer> itr = q.iterator();
-	    	 while(itr.hasNext())
-	    	 {
-	    		 if(itr.next() == su)
-	    		 {
-	    			 break; 
-	    		 }
-	    		 count++;
-	    	 }
-	    	 
-	    	 if(count<=(q.size()/2)) //q의 중간보다 앞에 있을경우 -> 2번이 빠름
-	    	 {
-	    		 for(int j=0;j<count;j++)
-	    		 {
-	    			 int imsi = q.poll();
-	    			 q.add(imsi);
-	    		 }
-		    	 System.out.println(count);
-	    		 ans += count;
-	    	 }
-	    	 else //3번
-	    	 {
-	    		 for(int j=0;j<q.size()-count;j++)
-	    		 {
-	    			 int imsi = q.removeLast();
-	    			 q.addFirst(imsi);
-	    		 }
-		    	 System.out.println(q.size()-count);
-	    		 ans += q.size()-count+1; //+1 이유는 1번 사용하기 위해 제일 앞으로 불러와야 하기 때문
-	    		 count3++;
-	    	 }
-	    	 System.out.println("q.peek :"+q.peek());
-	    	 q.poll(); //1번
-	     }
-	     if(count3>1)
-	    	 ans+=count3-2;
 	     bw.write(ans+"\n");
 	     bw.flush();
 }
+	 
+	 static void DFS(int y, int x, int count)
+	 {
+		 if(count ==4)
+		 {
+				System.out.println("["+y+","+x+"]"+" count : "+count);
+				chess[y][x]=false;
+				sero[y] = false;
+				garo[x] = false;
+			 ans++;
+		 }
+		 if(count==0) //그냥 두기
+			{
+				System.out.println("["+y+","+x+"]"+" count : "+count);
+				chess[y][x] = true; //해당 자리 표시
+				sero[y] = true; //i번째 줄에 뭐가 있음 
+				garo[x] = true; //가로 j칸에 뭐가 있음
+				if(y+1 <n)
+					DFS(y+1,x,count+1); //다음 안닿게 놓을 수 있는 가장 가까운 자리
+				else
+					return;
+				
+				//끝나고 돌아오면
+				chess[y][x]=false;
+				sero[y] = false;
+				garo[x] = false;
+			}
+		 else 
+		 {
+			for(int i=y;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
+				{
+					System.out.println("i : "+i+" j :"+j);
+					if(4-count > n-i) //남은 퀸 수보다 남은 줄 수가 더 작을 때
+					 {
+						return; 
+					 }
+					else //다른칸이랑 비교해야
+					{
+						if(sero[i] == true) //가로에 이미 있음, 다음줄로
+						{
+							break;	
+						}
+						else if(garo[j] == true) //세로에 이미 있음, 다음칸으로
+						{
+							continue;	
+						}
+						//대각선 비교
+						//1사분면
+						int imsiy = i-1;
+						int imsix = j+1;
+						
+						//1사분면
+						boolean isnext = false;
+						while(imsiy >=0 && imsix <n) //안튀어나가도록
+						{
+							if(chess[imsiy--][imsix++] == true) //이미 있음 -> 공격가능
+							{
+								isnext = true;
+								break;
+							}
+						}
+						if(isnext == true)
+							continue;
+						//2
+						imsiy = i-1;
+						imsix = j-1;
+						if(imsiy >=n || imsix >=n)
+							continue;
+						while(imsiy >=0 && imsix >=0) //안튀어나가도록
+						{
+							if(chess[imsiy--][imsix--] == true) //이미 있음 -> 공격가능
+							{
+								isnext = true;
+								break;	
+							}
+						}
+						if(isnext == true)
+							continue;
+						//3
+						imsiy = i+1;
+						imsix = j-1;
+						while(imsiy <n && imsix >=0) //안튀어나가도록
+						{
+							if(chess[imsiy++][imsix--] == true) //이미 있음 -> 공격가능
+							{
+								isnext = true;
+								break;	
+							}
+						}
+						if(isnext == true)
+							continue;
+						//4
+						imsiy = i+1;
+						imsix = j+1;
+						while(imsiy <n && imsix <n) //안튀어나가도록
+						{
+							if(chess[imsiy++][imsix++] == true) //이미 있음 -> 공격가능
+							{
+								isnext = true;
+								break;	
+							}
+						}
+						if(isnext == true)
+							continue;
+						
+						//다 비교해봤으나 공격할만한 대상 없음
+						{
+							System.out.println("["+i+","+j+"]"+" count : "+count);
+							if(count ==3) //이미 3개 찍음
+							{
+								//이번에 찍으면 4개
+								ans+=1;
+								continue;
+							}
+								
+							chess[i][j] = true; //해당 자리 표시
+							sero[i] = true;
+							garo[j] = true;
+							if(i+1 <n)
+								DFS(i+1,j,count+1); //다음 안닿게 놓을 수 있는 가장 가까운 자리
+							else 
+								return;
+							
+							//끝나고 돌아오면
+							chess[i][j]=false;
+							sero[i] = false;
+							garo[j] = false;
+						}
+					}
+				}
+			}
+		 }
+	 }
 }
 
