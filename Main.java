e55062eae16d3a1e488871b88d5ea93605f0724d
Hyeju Kil <mmow110@naver.com>
2021-07-14 13:28:24 +0900

    fail

--- Main.java
+++ Main.java
@@ -5,65 +5,46 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	static int n;
-	static HashMap<int[], Integer> su;
+	static int ans =-1;
+	static int su[];
 	
 	public static void main(String[] args) throws IOException {
 		
-		int a, b, c;
-		su = new HashMap<int[], Integer>();
-		while(true)
-		{
-			String[] inputs = br.readLine().split(" ");
-			a = Integer.parseInt(inputs[0]);
-			b = Integer.parseInt(inputs[1]);
-			c = Integer.parseInt(inputs[2]);
-			
-			if(a==-1 && b==-1 && c==-1)
-				break;
-			else
-			{
-				jegwi(a,b,c);
-				bw.write("w("+a+", "+b+", "+c+") = "+su.get(new int[] {a,b,c})+"\n");
-				
-			}
-		}
+		int t = Integer.parseInt(br.readLine());
+		su = new int[50001];
+		
+		Arrays.fill(su, -1);
+		
+		DFS(t,0);
+		bw.write(ans+"\n");
 		bw.flush();
 	}
 	
-	static int jegwi(int a,int b, int c)
+	static void DFS(int n, int count)
 	{
-		int[] imsis = new int[] {a,b,c};
-		System.out.println(a+" "+b+" "+c);
-		if(su.containsKey(imsis))
+		if(n == 0) //³¡
 		{
-			System.out.println("Á¸Àç");
-			return su.get(imsis);
+			if(count < ans || ans == -1)
+			{
+				ans = count;
+			}
+			return;
 		}
-		if(a<=0 || b<=0 || c<=0) 
+		else if(n<0)
+			return;
+		else if(su[n] != -1)
 		{
-			int imsi = 1;
-			su.put(imsis, imsi);
-			System.out.println("imsis : "+su.get(imsis));
-			return imsi;
+			if(count>su[n])
+				return;
 		}
-		else if(a>20 || b>20 || c>20)
+		 //´úÇÔ
 		{
-			int imsi = jegwi(20,20,20);
-			su.put(imsis, imsi);
-			return imsi; 
-		}
-		else if(a<b && b<c)
-		{
-			int imsi = jegwi(a,b,c-1)+jegwi(a,b-1,c-1)-jegwi(a,b-1,c);
-			su.put(imsis, imsi);
-			return imsi; 
-		}
-		else 
-		{
-			int imsi = jegwi(a-1,b,c)+jegwi(a-1,b,c-1)-jegwi(a-1,b-1,c-1);
-			su.put(imsis, imsi);
-			return imsi; 
+			su[n] = count;
+			int now = (int) Math.sqrt(n);
+			for(int i=now;i>0;i--)
+			{
+				DFS((int)(n-Math.pow(i, 2)), count+1);
+			}
 		}
 	}
 }
