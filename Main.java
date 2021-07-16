ba649e6d64aabe27fc5b8e849c577408ff12e8b4
Hyeju Kil <mmow110@naver.com>
2021-07-17 01:06:13 +0900

    rgb fail

--- Main.java
+++ Main.java
@@ -1,50 +1,110 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	static int ans =-1;
-	static int su[];
+
+	static long mem[];
+	static int[][] cost;
+	static int t;
 	
 	public static void main(String[] args) throws IOException {
 		
-		int t = Integer.parseInt(br.readLine());
-		su = new int[50001];
+		t = Integer.parseInt(br.readLine());
+		mem = new long[t+1];
+		cost = new int[t+1][t+1];
 		
-		Arrays.fill(su, -1);
+		Arrays.fill(mem, -1);
 		
-		DFS(t,0);
-		bw.write(ans+"\n");
-		bw.flush();
+		
+		for(int i=0;i<t;i++)
+		{
+			String s[] = br.readLine().split(" ");
+			
+			for(int j=0;j<t;j++)
+			{
+				cost[i][j] = Integer.parseInt(s[j]);
+			}
+			
+		}
+		
+		long ans =-1;
+		for(int i=0;i<3;i++)
+		{
+			long imsi = recursion(t, i);
+			if(ans ==-1 || ans<imsi)
+				ans = imsi;
+			System.out.println(imsi);
+			Arrays.fill(mem, -1);
+		}
+		
+//		bw.write(+"\n");
+//		bw.flush();
+		
 	}
 	
-	static void DFS(int n, int count)
+	static long recursion(int n,int before)
 	{
-		if(n == 0) //≥°
+		System.out.println("n : "+n+" before : "+before +" mem : "+mem[n] );
+		if(n<0 )
+			return 0;
+		else if(mem[n] !=-1)
+			return mem[n];
+		if(n==0)
 		{
-			if(count < ans || ans == -1)
+			if(before==0)
 			{
-				ans = count;
+				return mem[0] = min(cost[0][1],cost[0][2]);
 			}
-			return;
+			else if(before ==1)
+				return mem[0] = min(cost[0][0],cost[0][2]);
+			else
+				return mem[0] = min(cost[0][0],cost[0][1]);
 		}
-		else if(n<0)
-			return;
-		else if(su[n] != -1)
+		else
 		{
-			if(count>su[n])
-				return;
-		}
-		 //¥˙«‘
-		{
-			su[n] = count;
-			int now = (int) Math.sqrt(n);
-			for(int i=now;i>0;i--)
+			long imsi1,imsi2;
+			if(before ==0) //¿Ã¿¸ ª°
 			{
-				DFS((int)(n-Math.pow(i, 2)), count+1);
+				imsi1 = recursion(n-1, 1);
+				imsi2 = recursion(n-1, 2);
+				
+				if(imsi1<imsi2)
+					return mem[n] = imsi1+cost[n][1];
+				else
+					return mem[n] = imsi2+cost[n][2];
+			}
+			else if(before ==1)//√ 
+			{
+				imsi1 = recursion(n-1, 0);
+				imsi2 = recursion(n-1, 2);
+				
+				if(imsi1<imsi2)
+					return mem[n] = imsi1+cost[n][0];
+				else
+					return mem[n] = imsi2+cost[n][2];
+			}
+			else //∆ƒ where ==2
+			{
+				imsi1 = recursion(n-1, 0);
+				imsi2 = recursion(n-1, 1);
+				
+				if(imsi1<imsi2)
+					return mem[n] = imsi1+cost[n][0];
+				else
+					return mem[n] = imsi2+cost[n][1];
 			}
 		}
+	}
+	
+	static long min(long a, long b)
+	{
+		if(a<b)
+			
+			return a;
+		else
+			return b;
 	}
 }
