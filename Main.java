1501a30f680bbedf8a642cc17c67c7d090fd57df
Hyeju Kil <mmow110@naver.com>
2021-07-28 23:05:32 +0900

    imsi

--- Main.java
+++ Main.java
@@ -6,44 +6,32 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	static int n,k;
-	static long mem[];
-	static int coin[];
-	
 	public static void main(String argv[]) throws NumberFormatException, IOException
 	{
 		String s[] = br.readLine().split(" ");
-		n = Integer.parseInt(s[0]);
-		k = Integer.parseInt(s[1]);
+		int n = Integer.parseInt(s[0]);
+		int k = Integer.parseInt(s[1]);
+		int mem[] = new int[k+1];
 		
-		mem = new long[k+1];
-		coin = new int[n+1];
 		
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n;i++) //동전 수만큼 반복
 		{
-			coin[i] = Integer.parseInt(br.readLine());
-		}
-		for(int i=1;i<=k;i++) //수만큼
-		{
-			long min = 0;
-			for(int j=0;j<n;j++) //동전 종류만큼
+			int coin = Integer.parseInt(br.readLine());
+			mem[coin] +=1;
+			
+			for(int j=coin;j<=k;j++) //동전으로 카운팅
 			{
-				long imsi;
-				if(i-coin[j]>=0)
-				{
-					imsi = mem[i-coin[j]]+1;
-					if(imsi<min || min == 0)
-						min = imsi;
-				}
+				mem[j] += mem[j-coin];
 			}
 			
-			mem[i] = min;
+			for(int j=0;j<=k;j++)
+			{
+				System.out.print(mem[j]+" ");
+			}
+			System.out.println();
 		}
 		
-		if(mem[k] !=0)
-			bw.write(mem[k]+"\n");
-		else
-			bw.write(-1+"\n");
+		bw.write(mem[k]+"\n");
 		bw.flush();
 	}	
 }
